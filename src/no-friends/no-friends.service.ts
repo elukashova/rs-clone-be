9e0364cd75b5de5259f10527c986bafb172921f5
elukashova <ele.lukashova@gmail.com>
2023-02-25 01:10:01 +0100

    refactor: add data to no-friends response

--- src/no-friends/no-friends.service.ts
+++ src/no-friends/no-friends.service.ts
@@ -20,10 +20,89 @@ export class NoFriendsService {
         username: true,
         country: true,
         avatarUrl: true,
-        activities: true,
+        activities: {
+          select: {
+            id: true,
+            time: true,
+            date: true,
+            title: true,
+            elevation: true,
+            duration: true,
+            sport: true,
+            description: true,
+            distance: true,
+            companionId: true,
+            userId: true,
+            user: {
+              select: {
+                username: true,
+                avatarUrl: true,
+              },
+            },
+            kudos: {
+              select: {
+                userId: true,
+                user: {
+                  select: {
+                    avatarUrl: true,
+                  },
+                },
+                createdAt: true,
+              },
+            },
+            comments: {
+              select: {
+                id: true,
+                body: true,
+                createdAt: true,
+                updatedAt: true,
+                user: {
+                  select: {
+                    avatarUrl: true,
+                    username: true,
+                  },
+                },
+                userId: true,
+                likes: {
+                  select: {
+                    userId: true,
+                  },
+                },
+              },
+            },
+            route: {
+              select: {
+                id: true,
+                startPoint: true,
+                endPoint: true,
+                travelMode: true,
+                mapId: true,
+              },
+            },
+          },
+        },
       },
     });
     const filtered = notFriends.filter((user) => user.id !== id);
+    filtered.forEach((noFriend) => {
+      noFriend.activities.forEach((activity) => {
+        activity.kudos.map((kudo) => {
+          Object.assign(kudo, kudo.user);
+          delete kudo.user;
+        });
+
+        Object.assign(activity, activity.user);
+        delete activity.user;
+
+        activity.comments.map((comment) => {
+          Object.assign(comment, comment.user);
+          delete comment.user;
+
+          const newA = comment.likes.map((like) => like.userId);
+          Object.assign(comment.likes, newA);
+        });
+      });
+    });
     return filtered.map((person) => new FriendsResponseDto(person));
   }
 }
